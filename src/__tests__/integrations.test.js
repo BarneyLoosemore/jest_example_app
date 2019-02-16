5c9269f6af153eae671ab4b34d41c6d028beefe4
BarneyLoosemore <biscuitfull@gmail.com>
2019-02-16 11:24:02 +0000

    Added testing for fetching of tweets - asynchronous fetch request faked using fetchMock library

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,24 +1,54 @@
 import React from 'react'
 import { mount } from 'enzyme'
-import configureMockStore from 'redux-mock-store'
-import thunk from 'redux-thunk'
 import fetchMock from 'fetch-mock'
 
 import Root from 'Root'
 import App from 'components/App'
 
 
-it('can fetch a list of tweets and display them', () => {
+beforeEach(() => {
+  // intercept the global 'fetch' request/variable (where the request is made to this URL)
+  // and send a request equivalent to the body
+  fetchMock.get('http://jsonplaceholder.typicode.com/comments', {
+    body: [{ "name": "First fetched tweet"}, { "name": "Second fetched tweet" }]
+  })
+})
 
+afterEach(() => {
+  // reset/restore the global fetch variable (atleast w/ respect to the specified URL)
+  fetchMock.restore()
+})
+
+
+it('can fetch a list of tweets and display them', (done) => {
+
+  // declare a variable equivalent to the component architecture
+  // of the app
   const component = mount(
     <Root>
       <App />
     </Root>
   )
-
+  
+  // find the button with the class-name of 'fetch-tweets'
+  //  and simulate a click event on it
   component.find('.fetch-tweets').simulate('click')
+  
+  // allow time for the fetchMock request to be made, and 
+  // a response to be returned before test expectation executed
+  setTimeout(() => {
+    // force our application to update, i.e. display the newly fetched props as <li>'s
+    component.update()
 
-  expect(component.find('li').length).toEqual(500)
+    // expect the our new tweet list to be equal to the 
+    // number returned in the response from our fetchMock request
+    expect(component.find('li').length).toEqual(2)
 
+    // finish the test, & not prematurely 
+    done()
 
+    // unmount/cleanup our application component from our 
+    // test/CL DOM in order to avoid side-effects
+    component.unmount()
+  }, 100)
 })
